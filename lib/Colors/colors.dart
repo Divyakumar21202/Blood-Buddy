851599afd00f9b44247e683ca0d7d2883ec72352
DIVYAKUMAR PATEL <124331485+Divyakumar21202@users.noreply.github.com>
2024-03-28 15:52:26 +0530

    chore : updated master

--- lib/Colors/colors.dart
+++ lib/Colors/colors.dart
@@ -14,6 +14,6 @@ Color primaryColor = const Color.fromRGBO(255, 193, 7, 1);
 //Color(#E91E63 as int);
 Color textColor = Color(#FFFFFF as int);
 Color accentColor = Color(#FF4081 as int);
-Color primaryTextColors = Color.fromRGBO(117, 117, 117, 1);
-Color secondaryColor = Color.fromARGB(255, 33, 33, 33);
+Color primaryTextColors =const Color.fromRGBO(117, 117, 117, 1);
+Color secondaryColor =const Color.fromARGB(255, 33, 33, 33);
 Color dividerColor = Color(#BDBDBD as int);
